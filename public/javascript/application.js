4bf0a64b4d76bb9c1c9c1ea6b19616a8b8675485
Hakimu Jackson <hakimu@newrelic.com>
2017-02-25 15:27:12 -0800

    Remove comments

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -3,12 +3,5 @@ $(document).ready(function() {
   $("#products_box").keypress(function(){
     var text = $(this).text();
     $(".test").append("foo");
-    // console.log(typeof(text));
-    // $("#ounces_box").val(function(index,value){
-    //   var val = value
-    //   return val
-    // });
-    // console.log($(this).val());
-    // console.log("Keypress the id!");
   });
 });
