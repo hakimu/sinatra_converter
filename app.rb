9b53205deb9ac9451ce9cf5626e6b95445befbd6
Hakimu Jackson <hakimu@newrelic.com>
2017-02-25 15:25:37 -0800

    JavaScript testing

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
+require_relative './lib/converter'
 require 'sinatra'
+require 'newrelic_rpm'
 
 get '/' do
   erb :home
 end
 
 get '/convert' do
-  @foo = params[:foo]
-  @bar = params[:bar]
+  @lbs = params[:pounds].to_i
+  @oz = params[:ounces].to_i
+  @lbs_amount = Conversion.new.pounds_to_ounce(@lbs)
   erb :convert
 end
 
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   console.log( "jquery is loaded!" );
+  $("#products_box").keypress(function(){
+    var text = $(this).text();
+    $(".test").append("foo");
+    // console.log(typeof(text));
+    // $("#ounces_box").val(function(index,value){
+    //   var val = value
+    //   return val
+    // });
+    // console.log($(this).val());
+    // console.log("Keypress the id!");
+  });
 });
